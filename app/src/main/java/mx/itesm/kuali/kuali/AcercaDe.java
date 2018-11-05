2d8d730e557492a192055753941772449e9a33e8
Servio Tulio Reyes Castillo <servio_trc@yahoo.com.mx>
2018-11-04 18:56:52 -0600

    Cambio en el Acerca De

--- app/src/main/java/mx/itesm/kuali/kuali/AcercaDe.java
+++ app/src/main/java/mx/itesm/kuali/kuali/AcercaDe.java
@@ -7,10 +7,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class AcercaDe extends AppCompatActivity {
 
+    TextView correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,11 @@ public class AcercaDe extends AppCompatActivity {
         setContentView(R.layout.activity_acerca_de);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        correo = findViewById(R.id.correo_contacto);
+        correo.setText(Html.fromHtml("<a href=\"mailto:app.kuali.mx@gmail.com\">app.kuali.mx@gmail.com</a>"));
+        correo.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    public void registro_de_productos(View v){
-        Intent tabMain = new Intent(getBaseContext(), Formulario_Producto.class);
-        startActivity(tabMain);
-    }
+
 
 }
